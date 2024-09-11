dac64fa966cb8fd754ef4377e8173f3691b13e5b
Ratnesh Kumar <jnvratneshkumar@gmail.com>
2024-09-11 07:27:51 +0530

    Update FCFS

--- Practical/C++/First Come First Serve.cpp
+++ Practical/C++/First Come First Serve.cpp
@@ -3,17 +3,20 @@
 
 #include <iostream>
 void First_Come_First_Serve(std::string name_of_process[], int burst_time[],int number_of_processes){
-    std::cout<<"Name Of Process\tBurst Time\tWaiting Time\tTurnAround Time"<<std::endl;
     
     double waiting_time = 0;
     double turnaround_time = 0;
     double total_waiting_time = 0;
     double total_turnaround_time = 0;    
+
+    std::cout<<"Name Of Process\tBurst Time\tWaiting Time\tTurnAround Time"<<std::endl;
+
     for (int i=0;i<number_of_processes;i++){
         turnaround_time += burst_time[i];
         total_turnaround_time+=turnaround_time;
         
        std::cout<<name_of_process[i]<<"\t\t"<<burst_time[i]<<"\t\t"<<waiting_time<<"\t\t"<<turnaround_time<<std::endl;
+       
        total_waiting_time+=waiting_time;
        waiting_time += burst_time[i];
     }
@@ -22,15 +25,18 @@ void First_Come_First_Serve(std::string name_of_process[], int burst_time[],int 
 
 int main(){
     int number_of_processes;
+
     std::cout<<"Enter the number of processes: ";
     std::cin>>number_of_processes;
     std::string name_of_process[number_of_processes];
     int burst_time[number_of_processes];
+
     for (int i=0;i<number_of_processes;i++){
         std::cout<<"Enter Name of Process:: ";
         std::cin>>name_of_process[i];
         std::cout<<"Enter Burst Time of Process "<<name_of_process[i]<<":: ";
         std::cin>>burst_time[i];
     }
+
     First_Come_First_Serve(name_of_process,burst_time,number_of_processes);
 }
--- Practical/Python/First Come First Serve.py
+++ Practical/Python/First Come First Serve.py
@@ -1,29 +1,37 @@
 # First Come First Serve Scheduling Algorithm (FCFS)
 
 
-def First_Come_First_Serve(name_of_process,burst_time,number_of_processes):
-    print("Name Of Process\tBurst Time\tWaiting Time\tTurnAround Time")
-    
+def First_Come_First_Serve(name_of_process, burst_time, number_of_processes):
+
     waiting_time = 0
-    turnaround_time = 0
     total_waiting_time = 0
     total_turnaround_time = 0
 
+    print("Name Of Process\tBurst Time\tWaiting Time\tTurnAround Time")
+
     for i in range(len(name_of_process)):
-        turnaround_time += burst_time[i]
-        total_turnaround_time+=turnaround_time
+        turnaround_time = waiting_time + burst_time[i]
+        total_turnaround_time += turnaround_time
+
         print(f"{name_of_process[i]}\t\t{burst_time[i]}\t\t{waiting_time}\t\t{turnaround_time}")
-        total_waiting_time+=waiting_time
+        
+        total_waiting_time += waiting_time
         waiting_time += burst_time[i]
-    print(f"\nAverage Waiting Time is:: {total_waiting_time/number_of_processes}\nAverage TurnAround Time is:: {total_turnaround_time/number_of_processes}")
+    print(f"\nAverage Waiting Time is:: {total_waiting_time / number_of_processes}\nAverage TurnAround Time is:: {total_turnaround_time / number_of_processes}")
 
-name_of_process = []
-burst_time = []
 
-number_of_processes = int(input("Enter the number of processes: "))
+def main():
+    name_of_process = []
+    burst_time = []
 
-for i in range(number_of_processes):
-    name_of_process.append(input(f"Enter name of process {i + 1}: "))
-    burst_time.append(int(input(f"Enter burst time for process {name_of_process[i]}: ")))
+    number_of_processes = int(input("Enter the number of processes: "))
 
-First_Come_First_Serve(name_of_process, burst_time,number_of_processes)
+    for i in range(number_of_processes):
+        name_of_process.append(input(f"Enter name of process {i + 1}: "))
+        burst_time.append(int(input(f"Enter burst time for process {name_of_process[i]}: ")))
+
+    First_Come_First_Serve(name_of_process, burst_time, number_of_processes)
+
+
+if __name__ == "__main__":
+    main()
